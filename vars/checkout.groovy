8c1347d9bd442bde7a7767ca9dfdccbf7dd1a6d6
7nmanoj <91119008+7nmanoj@users.noreply.github.com>
2022-05-17 09:29:19 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -8,7 +8,7 @@ def call(Map pipelineParams) {
                       println "Approval."    
 
                       checkout([$class: 'GitSCM',
-                                        branches: [[name: 'master']],
+                                        branches: [[name: '*/master']],
                                         url: pipelineParams.url])
                       println "Success"
                         
