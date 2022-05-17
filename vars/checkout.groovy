8ae1e7cd6de4b804179157af91acb150853057cf
7nmanoj <91119008+7nmanoj@users.noreply.github.com>
2022-05-17 19:38:33 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,8 +1,28 @@
 def call(Map stageParams) {
+   pipeline {
+        agent any
+        stages {
+            stage('Git Checkout') {
+                steps {
+                checkout(
+                    branch: "main",
+                    url: "https://github.com/7nmanoj/shared-demo.git"
+                )
+                }
+            }
+        }
+   }
+}
+ 
+    
+    
+    
+    
+    
 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
+//     checkout([
+//         $class: 'GitSCM',
+//         branches: [[name:  stageParams.branch ]],
+//         userRemoteConfigs: [[ url: stageParams.url ]]
+//     ])
+//   }
